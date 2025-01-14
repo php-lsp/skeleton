3f7776c11104e8ec5fed8e4e73feb90b6652e6ae
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2025-01-14 05:01:03 +0300

    Use doc manager ext

--- app/Application.php
+++ app/Application.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace App;
 
+use Lsp\Extension\DocumentManager\DocumentManagerExtension;
 use Lsp\Kernel\LanguageServerKernel;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-final class Application extends LanguageServerKernel {}
+final class Application extends LanguageServerKernel
+{
+    protected function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->registerExtension(new DocumentManagerExtension());
+    }
+}
